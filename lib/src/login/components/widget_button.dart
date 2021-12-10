80188cdbab6542fc036a980a1464b0f6992e4542
LeThienIT <55796074+JoNSn0wX@users.noreply.github.com>
2021-12-10 22:48:39 +0700

    update widget_button

--- lib/src/login/components/widget_button.dart
+++ lib/src/login/components/widget_button.dart
@@ -1,24 +1,35 @@
-// ignore_for_file: must_be_immutable, prefer_typing_uninitialized_variables
-
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class WidgetButton extends StatelessWidget {
-  WidgetButton({Key? key, required this.name, required this.colors, required this.onTap}) : super(key: key);
+  WidgetButton(
+      {Key? key, required this.name, required this.colors, required this.onTap})
+      : super(key: key);
+  // ignore: prefer_typing_uninitialized_variables
   final name;
   Color colors;
+  // Icon icons;
   Function onTap;
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        onTap();
-      },
-      child: Container(
-        height: 40,
-        width: double.infinity,
-        color: colors,
-        child: Center(child: Text(name)),
-      ),
-    );
+        onTap: () {
+          onTap();
+        },
+        child: Center(
+          child: ElevatedButton.icon(
+            onPressed: () => {},
+            icon: const Icon(
+              Icons.login,
+              size: 32,
+            ),
+            label: Text(name),
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(double.infinity, 45),
+              primary: Colors.green,
+              onPrimary: Colors.white,
+            ),
+          ),
+        ));
   }
 }
